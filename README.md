08260c85421418b89254144e01f613607fba3913
Kurlgargyey <nahsirkivar@gmail.com>
2022-06-02 16:01:58 +0200

    Doc and some cleanups

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # odin-linked-list
+
+This is my implementation of a Linked List type in Ruby, made for the The Odin Project online course.
+
+The class has the following methods:
+
+## #append(value)
+Adds the given value as trailing to the list. Returns self.
+
+## #prepend(value)
+Adds the given value as leading to the list. Returns self.
+
+## #insert_at(idx, *values)
+Inserts the given value before the existing element at the specified index. Returns self.
+
+If index is out of bounds, extends the list.
+
+If index is negative, adds the given value after the elements at the specified index.
+
+Accepts multiple arguments.
+
+## #remove_at(idx)
+Removes the Node at the given index. Returns the deleted node.
+
+## #concat(linked_list)
+Appends the given linked list. Returns self.
+
+## #size
+Returns the number of nodes in the list.
+
+## #head
+Returns the leading node.
+
+## #tail
+Returns the trailing node.
+
+## #at(idx)
+Returns the node at the given index.
+
+## #pop
+Removes and returns the trailing node.
+
+## #contains?(value)
+Returns true if the linked list contains a node with the given value and false otherwise.
+
+## #find(value)
+Returns the index of the node with the given value if there is one and nil otherwise.
+
+## #to_s
+Returns a string representation of the list according to the schema:
+( head.value ) -> (next_node.value) ... -> ( tail.value ) -> nil
+
+## #each
+Enumerates through each node with the given block. If no block was given, returns an Enumerator.
--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
+# this class contains all methods related to managing a list of related nodes
 class LinkedList
   attr_reader :nodes
 
   def initialize(*args)
     @nodes = []
     args.each do |arg|
-      self.append(arg)
+      if arg.is_a?(Array)
+        arg.each do |arg_element|
+          append(arg_element)
+        end
+      else
+        append(arg)
+      end
     end
   end
 
@@ -24,27 +31,34 @@ class LinkedList
 
   def insert_at(idx, *values)
     # if we split the array, we can take care of out-of-bounds or negative indexes at this point
-    first_half = @nodes[0..idx-1].to_a
-    second_half = @nodes[idx..-1].to_a
+    first_half = LinkedList.new(@nodes[0..idx - 1].to_a.map(&:value))
+    second_half = LinkedList.new(@nodes[idx..-1].to_a.map(&:value))
+    first_half.append(second_half.nodes.shift.value) if idx.negative?
 
     # now we just need to append to the first half of the split array
     # would be nice to just make the halves linked lists as well, but i don't have a #concat yet
     values.each do |value|
-      first_half << Node.new(value)
-      first_half[-2] ? first_half[-2].next_node = first_half[-1] : return
+      first_half.append(value)
+      #first_half[-2] ? first_half[-2].next_node = first_half[-1] : next
     end
 
     # at the end, we just have to fix one next_node entry and concat
-    first_half[-1].next_node = second_half[0]
+    #first_half[-1].next_node = second_half[0]
     @nodes = first_half.concat(second_half)
 
     return self
   end
 
   def remove_at(idx)
-    @nodes.delete_at(idx)
-    # gotta remember to fix the next_node references if we remove a node
-    @nodes[idx-1] ? @nodes[idx-1].next_node = @nodes[idx] : return
+    deleted = @nodes.delete_at(idx)
+    # gotta fix the next_node references after we remove the node
+    @nodes[idx - 1] ? @nodes[idx - 1].next_node = @nodes[idx] : return
+    return deleted
+  end
+
+  def concat(linked_list)
+    tail.next_node = linked_list.head
+    @nodes.concat(linked_list.nodes)
     return self
   end
 
@@ -89,34 +103,35 @@ class LinkedList
 
   # needed a way to iterate for some integrity checking later
   def each
-    if block_given?
-      @nodes.each do |node|
-        yield node
-      end
-    else
-      return @nodes.each
+    return @nodes.each unless block_given?
+
+    @nodes.each do |node|
+      yield node
     end
   end
 end
 
+# this class defines an object which can keep track of a value and
+# one related object of the same type
 class Node
   attr_accessor :next_node
 
-  def initialize(value=nil, next_node=nil)
+  def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
   end
-  
+
   def value
     return @value
   end
 
   def to_s
-    return "( #{value} ) -> #{ next_node ? '( ' + next_node.value.to_s + ' )' : 'nil' }"
+    return "( #{value} ) -> #{next_node ? "( #{next_node.value} )" : 'nil'}"
   end
 end
 
-=begin def integrity_check(linked_list)
+
+def integrity_check(linked_list)
   previous_node = Node.new(nil,linked_list.head)
   index = 0
 
@@ -148,20 +163,24 @@ puts "Popped tail: #{list.pop}"
 puts "List contains 54? #{list.contains?(54)}"
 puts "Index of 9: #{list.find(9)}"
 puts "Index of 10: #{list.find(10)}"
-puts "Remove fifth node:"
-puts list.remove_at(4)
+puts list
+puts "Remove third node:"
+puts list.remove_at(2)
 
 puts "Integrity check: "
 integrity_check(list)
 
+puts "Insert 22, 33 second to last: "
 list.insert_at(-2, 22, 33)
 puts list
 
 puts "Integrity check: "
 integrity_check(list)
 
+puts "Insert 5, 8 in second place:"
 list.insert_at(1, 5, 8)
 puts list
 
 puts "Integrity check: "
-integrity_check(list) =end
+integrity_check(list)
+
